d30d69cfa42901b60800e01be2c0de7a8d6b0c48
Luke <luke.kingsley@inl.gov>
2017-06-26 11:00:44 -0600

    New Changes to VRPNInput for Wiimote

--- Plugin Files/Assets/UniCave/Scripts/VRPNInput.cs
+++ Plugin Files/Assets/UniCave/Scripts/VRPNInput.cs
@@ -14,13 +14,20 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class VRPNInput : MonoBehaviour
 {
+    public bool Cave = false;
+
+
     [SerializeField]
-    private string trackerAddress = "Wand0@C6_V1_HEAD";
-    private int channelVertical = 1;
-    private int channelHorizontal = 0;
+    private string trackerAddress = "WiiMote0@localhost";
+    private int channelVertical = 22;
+    private int channelHorizontal = 21;
+    private int channelMagnitude = 20;
     [SerializeField]
     private int channel = 1; //channel for buttons, not analog
     [SerializeField]
@@ -28,22 +35,109 @@ public class VRPNInput : MonoBehaviour
     [SerializeField]
     private bool trackAnalog = true;
 
+    private DriveTool driver;
+
     public GameObject wandObject = null;
     public bool debugOutput = false;
     public int numButtons = 6;
     public float movementSpeed = 0.01f;
-    public float rotationSpeed = 0.1f;
-    public double deadZone = 0.5;
-    public bool restrictVerticalMovement = true;
-
+    public float rotationSpeed = 0.05f;
+    public double deadZone = 0.05;
+    public Text tool;
+    public GameObject canvas;
     public GameObject holder;
-  
+    private ToolManager2 toolManager;
+    private Vector3 origin;
+    private Vector3 direction;
+    public float rayLength = 200;
+    bool[] buttonState;
+    bool curValue;
+    RaycastHit hit;
+    RaycastHit tester;
+    Vector3 offset;
+    bool hasStarted = false;
+    RaycastHit temp;
+    public GameObject cylinder;
+    public Material red;
+    public Material green;
+    public GameObject panel;
+    public Event eventsystem;
+
     public int Channel
     {
         get { return channel; }
         set
         {
             channel = value;
+        }
+    }
+
+
+    //private void Update()
+    //{
+    //    toolName();
+    //}
+
+    /// <summary>
+    /// Show the user what tool is currently selected.
+    /// </summary>
+    public IEnumerator toolName()
+    {
+        while (true)
+        {
+            if (toolManager.toolNumber == 0)
+            {
+                tool.text = "Tool: Warp";
+                //line.material.mainTexture = texture;
+            }
+
+            else if (toolManager.toolNumber == 1)
+            {
+                tool.text = "Tool: Grabber";
+                //cylinder.GetComponent<Renderer>().material.color = 
+            }
+
+            else if(toolManager.toolNumber == 2)
+            {
+                tool.text = "Tool: Clicker";
+            }
+            else
+            {
+
+            }
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator buttonInput()
+    {
+
+        while (true)
+        {
+            if(toolManager.toolNumber == 2)
+            {
+                Physics.Raycast(origin, direction, out tester);
+                if (tester.collider != null)
+                {
+                
+                if(tester.collider.tag == "button")
+                    {
+                        Button button = tester.transform.gameObject.GetComponent<Button>();
+                        EventSystem.current.SetSelectedGameObject(button.gameObject);
+                    }
+                else if (tester.collider.tag == "handle")
+                    {
+                        Button button = tester.transform.gameObject.GetComponent<Button>();
+                        EventSystem.current.SetSelectedGameObject(button.gameObject);
+                    }
+                }
+                else
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                }
+            }
+            yield return null;
         }
     }
 
@@ -75,8 +169,47 @@ public class VRPNInput : MonoBehaviour
         }
     }
 
+    private IEnumerator colorChange()
+    {
+        while (true)
+        {           
+            if (Physics.Raycast(origin, direction))
+            {
+                cylinder.GetComponent<Renderer>().material = green;
+            }
+            else
+            {
+                cylinder.GetComponent<Renderer>().material = red;
+            }
+            yield return new WaitForSeconds(.15f);
+        }
+    }
+
     private void Start()
     {
+        EventSystem.current.SetSelectedGameObject(null);
+        //Create a driver object to handle movement
+        driver = new DriveTool(deadZone, rotationSpeed, movementSpeed);
+        //Add a toolManager to the wandObject to shuffle between tools
+        wandObject.AddComponent<ToolManager2>();
+        //toolManager = wandObject.GetComponent<ToolManager2>();
+        toolManager = new ToolManager2(wandObject, holder);
+
+        buttonState = new bool[numButtons];
+
+        cylinder.GetComponent<Renderer>().material = red;
+
+        for (int i = 0; i < numButtons; i++)
+        {
+            buttonState[i] = false;
+        }
+
+        if (Cave)
+        {
+            //Change the address to the remote in the cave
+            //Switch the channels to match those of the Cave remote
+        }
+
         //this gets rid of this object from non-head nodes...we only want this running on the machine that connects to VRPN...
         //this assumes a distributed type setup, where one machine connects to the tracking system and distributes information
         //to other machines...
@@ -84,9 +217,9 @@ public class VRPNInput : MonoBehaviour
         //in that case, we wouldn't want to destroy this object..
         if (System.Environment.MachineName != MasterTrackingData.HeadNodeMachineName)
         {
-                Debug.Log("Removing tracker settings from " + System.Environment.MachineName);
-                Destroy(this);
-                return;
+            Debug.Log("Removing tracker settings from " + System.Environment.MachineName);
+            Destroy(this);
+            return;
         }
 
         if (trackButton)
@@ -97,98 +230,132 @@ public class VRPNInput : MonoBehaviour
         if (trackAnalog)
         {
             StartCoroutine("Analog");
+            StartCoroutine("colorChange");
+            StartCoroutine("toolName");
+            StartCoroutine("Raycaster");
+            StartCoroutine("buttonInput");
         }
     }
 
+    /// <summary>
+    /// Asynchronous method taking in button input and sending it to the current selected tool
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Button()
     {
         while (true)
         {
-            for(int i = 0; i < numButtons; ++i)
+            //i tracks the number of the current button
+            for (int i = 0; i < numButtons; ++i)
             {
+                curValue = VRPN.vrpnButton(trackerAddress, i);
+
+                if (buttonState[i] && !curValue)
+                {
+                    toolManager.list[toolManager.toolNumber].ButtonClick(i, origin, direction);
+                    driver.ButtonClick(i, origin, direction);
+
+                    hasStarted = false;
+                    hit = temp;
+
+                }
+                else if (buttonState[i] && curValue && i == 4)
+                {
+                    if (hit.distance > 0)
+                    {
+                        if (!hasStarted)
+                        {
+                            offset = hit.transform.position - hit.point;
+                            hasStarted = true;
+                        }
+
+                        if (toolManager.toolNumber == 2)
+                        {
+                            toolManager.list[toolManager.toolNumber].ButtonClick(i, origin, direction);
+                        }
+                        else
+                        {
+                            toolManager.list[toolManager.toolNumber].ButtonDrag(hit, offset, origin, direction);
+                        }
+                    }
+                }
+
+                else if (!buttonState[i] && curValue && i == 4)
+                {
+                    Physics.Raycast(origin, direction * rayLength, out hit);
+                }
+
+                buttonState[i] = curValue;
+
+                //Sends the button currently being iterated over and whether or not it is being pressed
+
+
+                //Handles all movement of the wandObject
+
+
+                //Change between tools
                 if (VRPN.vrpnButton(trackerAddress, i))
                 {
-                    if (debugOutput)
+                    switch (i)
                     {
-                        Debug.Log("Button " + i + " pressed on channel " + channel);
+                        case 5:
+                            toolManager.PreviousTool();
+                            yield return new WaitForSeconds(.2f);
+                            break;
+                        case 6:
+                            toolManager.NextTool();
+                            yield return new WaitForSeconds(.2f);
+                            break;
+                        default:
+                            break;
                     }
-                    yield return new WaitForSeconds(.2f);
+                    // yield return new WaitForSeconds(.02f);
                 }
             }
-           
             yield return null;
         }
     }
 
+    /// <summary>
+    /// Asyncronous method that handles all the input from the analog stick
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Analog()
     {
-        //some basic default analog wand movement.
-        if (wandObject == null)
-        {
-            wandObject = GameObject.Find("Wand");
-        }
-
-        Vector3 modTrans;
         while (true)
         {
+            //Get the X and Y values from the joystick
             double analogVertical = VRPN.vrpnAnalog(trackerAddress, channelVertical);
             double analogHorizontal = VRPN.vrpnAnalog(trackerAddress, channelHorizontal);
-            if (analogVertical >= deadZone || analogVertical <= -deadZone)
-            {
-                if (debugOutput)
-                {
-                    Debug.Log("Analog input value " + analogVertical + " on channel " + channelVertical);
-                }
 
-                if (holder != null)
-                {
-                   
-                    if (wandObject != null)
-                    {
-                        modTrans = wandObject.transform.localRotation * Vector3.forward * movementSpeed;
-                    }
-                    else 
-                    {
-                        modTrans = Vector3.forward * movementSpeed;
-                    }
-
-                    if (restrictVerticalMovement)
-                    {
-                        modTrans.y = 0;
-                    }
-
-                    if (analogVertical >= deadZone)
-                    {   //moving forward
-                        holder.transform.Translate(modTrans);
-                    }
-                    else
-                    {   //moving back
-                        holder.transform.Translate(-modTrans);
-                    }
-                }
-            }
-            if (analogHorizontal >= deadZone || analogHorizontal <= -deadZone)
-            {
-                if (debugOutput)
-                {
-                    Debug.Log("Analog input value " + analogHorizontal + " on channel " + channelHorizontal);
-                }
-
-                if (holder != null)
-                {
-
-                    if (analogHorizontal >= deadZone)
-                    {   //rotating right
-                        holder.transform.Rotate(new Vector3(0, rotationSpeed, 0));
-                    }
-                    else
-                    {   //rotating left
-                        holder.transform.Rotate(new Vector3(0, -rotationSpeed, 0));
-                    }
-                }
-            }
+            driver.Analog(analogHorizontal, analogVertical);
 
             yield return null;
         }
     }
+
+    /// <summary>
+    /// Asyncronous method that continually casts a Raycast from the wands direction
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Raycaster()
+    {
+        //Debug.Log("!");
+        //Continually update the wandObjects direction and position
+        while (true)
+        {
+            //Get the origin of the object
+            origin = wandObject.transform.position;
+
+            //Get the direction of the object
+            direction = wandObject.transform.forward;
+
+            //Create the RayCast and Draw it for debugging purposes
+            //Physics.Raycast(origin, direction * rayLength);
+            Debug.DrawRay(origin, direction, Color.black);
+
+            yield return null;
+        }
+    }
+
 }
